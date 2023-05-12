3bbe8b49b4fcf0b86809aac0678f508b761b3a56
Diego-Bonora <bonoradi@gmail.com>
2023-05-12 08:51:47 -0700

    fixed to properly count the leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,15 +8,12 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
-
 	if (tree)
 	{
-		if (tree->left)
-			leaves++;
-		if (tree->right)
-			leaves++;
+		if (!tree->left && !tree->right)
+			return (1);
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 	}
-	return (leaves);
+	return (0);
 }
 
