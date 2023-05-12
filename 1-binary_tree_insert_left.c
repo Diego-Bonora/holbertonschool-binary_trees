4359f84a1644b642e084e9df8dd693be06f7332a
root <lautarorodriguezcoitinho@gmail.com>
2023-05-11 20:36:17 -0700

    Task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+binary_tree_t
+*binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	if (parent->left != NULL)
+	{
+		parent->left->parent = new;
+		new->left = parent->left;
+	}
+
+	parent->left = new;
+
+	return (new);
+}
+
+
