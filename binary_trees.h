b5011548a64c17d9b6c4b5f8d46044a9f6e020d7
Diego-Bonora <bonoradi@gmail.com>
2023-05-12 06:47:42 -0700

    new prototype addeed

--- binary_trees.h
+++ binary_trees.h
@@ -32,5 +32,6 @@ void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* _BINARY_TREES_H_ */
