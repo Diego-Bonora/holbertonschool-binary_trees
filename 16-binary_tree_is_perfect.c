273ffdf0841f5a187978c5a3d232d12442671144
Diego-Bonora <bonoradi@gmail.com>
2023-05-15 09:51:22 -0700

    reorganiced function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,36 +2,58 @@
 #include "stdbool.h"
 
 int
-count_nodes(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		int left_count = count_nodes(tree->left);
-		int right_count = count_nodes(tree->right);
-
-		return (1 + left_count + right_count);
-	}
-}
-
-int
 binary_tree_is_perfect(const binary_tree_t *tree)
 {
-
-	int left_count;
-	int right_count;
-
 	if (tree == NULL)
 		return (0);
 
-	left_count = count_nodes(tree->left);
-	right_count = count_nodes(tree->right);
-
-	if (left_count == right_count)
+	if (binary_tree_is_full(tree) && (binary_tree_height(tree->left) == binary_tree_height(tree->right)))
 		return (1);
+	return (0);
+}
 
+/**
+ * binary_tree_is_full - checks if the binary tree is full or not
+ * @tree: imput node
+ * Return: returns 1 if its a full tree else 0
+*/
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if (!tree->left && !tree->right)
+			return (1);
+		if (tree->left && tree->right)
+			return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+		return (0);
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_height - finds and returns the height of the tree
+ * @tree: head node
+ * Return: returns the height of the tree
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left;
+	size_t right;
+
+	if (tree)
+	{
+		if (tree->left || tree->right)
+		{
+			left = binary_tree_height(tree->left);
+			right = binary_tree_height(tree->right);
+			if (left >= right)
+				return (left + 1);
+			else
+				return (right + 1);
+		}
+	}
 	return (0);
 }
